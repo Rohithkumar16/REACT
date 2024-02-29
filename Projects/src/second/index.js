f01b5b86ec199a74bfc0a07ea5550118664f5f16
Rohithkumar16 <rohithkumarthokala77@gmail.com>
2024-03-01 00:14:23 +0530

    gitdemoo

--- Projects/src/second/index.js
+++ Projects/src/second/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDom from "react-dom/client";
 import { Books } from "./books";
 import Bookslist from "./bookslist";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    Bookslist
+    <Bookslist/>
 );
